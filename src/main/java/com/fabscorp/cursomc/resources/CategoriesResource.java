e51fb41c79d3fba6737b887926496732709cb674
Fabricio Trindade <fabrtrindade@gmail.com>
2023-02-08 09:12:00 -0300

    Testing domain first class

--- src/main/java/com/fabscorp/cursomc/resources/CategoriesResource.java
+++ src/main/java/com/fabscorp/cursomc/resources/CategoriesResource.java
@@ -1,16 +1,30 @@
 package com.fabscorp.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fabscorp.cursomc.domain.Category;
 
 @RestController
 @RequestMapping (value = "/categories")
 public class CategoriesResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is running!";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(1, "Office");
+		
+		List <Category> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 
